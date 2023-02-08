aec96da4843f9bbfbb6817bd766f708b9d900b00
yphill <phelipequeiroz2004@gmail.com>
2023-02-07 21:24:23 -0300

    4º parte

--- app/src/main/java/br/com/etecia/yourshoes/MainActivity.java
+++ app/src/main/java/br/com/etecia/yourshoes/MainActivity.java
@@ -3,6 +3,10 @@ package br.com.etecia.yourshoes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,10 +23,35 @@ public class MainActivity extends AppCompatActivity {
 
         idLista = findViewById(R.id.idLista);
 
-        //idLista.setAdapter();
+        MyAdapter adapter = new MyAdapter();
 
+        idLista.setAdapter(adapter);
 
 
     }
+
+    public class MyAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
+    }
+
 
 }
